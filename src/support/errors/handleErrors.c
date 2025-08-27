5ffb46f75e65aa07f9b66f7230ef582e54fc6514
Morgan Williams <morgachilla2018@gmail.com>
2025-08-27 01:46:07 -0500

    Wrote error handling with descriptive output that uses system errno when they exist and custom otherwise. Not yet tested.

--- src/support/errors/handleErrors.c
+++ src/support/errors/handleErrors.c
@@ -1,2 +1,54 @@
 #include "handleErrors.h"
 #include "textStylesAndColors.h"
+#include <stdio.h>
+#include <string.h>
+
+static const char* err_getErrName(int err);
+static const char* err_getErrDescription(int err);
+
+int err_reportError(
+                ErrorFatality isErrFatal, int err,
+                const char* file, int line, const char* func) {
+        // Build the error splash
+        fprintf(stderr, FG_RED "[");
+        if (isErrFatal == ERR_FATAL) fprintf(stderr, BOLD "FATAL_");
+        fprintf(stderr, "%s" UNBOLD "]" RESET ": ", err_getErrName(err));
+
+        // Print diagnostic information
+        fprintf(stderr, "FILE: %s; LINE: %d; IN %s, %s\n", 
+                file, line, func, err_getErrDescription(err));
+        
+        // Exit on fatal error. Return errno for sys errors and 1 for custom
+        int exitStatus = err < 256 ? err : 1;
+        if (isErrFatal) inChild ? _exit(exitStatus) : exit(exitStatus);
+
+        return 0;
+}
+
+static const char* err_getErrName(int err) {
+        #define X(name, val) case name: return (#name + ERRNO_PREFIX_SLICE);
+        switch (err) {
+                SYS_ERRORS
+                CUSTOM_ERRORS
+                default: return "ERR_UNKNOWN";
+        }
+        #undef X
+}
+
+static const char* err_getErrDescription(int err) {
+        if (err == ERRNO_NONE) return "Not an error";
+        if (err < 1000) return strerror(err); // System errors
+
+        // Custom errors
+        static const char* tbl[] = {
+                [ERRNO_SYNTAX - 1000] = "There is a problem with your input",
+                [ERRNO_OVERFLOW - 1000] = "Request too large"
+
+        };
+
+        // Don't fall off the table and avoid NULL slots
+        int tblLen = sizeof(tbl) / sizeof(tbl[0]);
+        int index = err - 1000;
+        if (index >= 0 && index < tblLen && tbl[index]) return tbl[index];
+        return "Unknown error";
+}
--- src/support/errors/handleErrors.h
+++ src/support/errors/handleErrors.h
@@ -1,6 +1,37 @@
 #ifndef HANDLE_ERRORS_H
 #define HANDLE_ERRORS_H
 
+#include <errno.h>
+#include <stdbool.h>
 
+extern bool inChild;
+
+typedef enum {
+        ERR_NONFATAL,
+        ERR_FATAL
+} ErrorFatality;
+
+#define ERRNO_PREFIX_SLICE 6
+
+#define SYS_ERRORS \
+        X(ERRNO_EPERM, EPERM) \
+        X(ERRNO_EAGAIN, EAGAIN) \
+        X(ERRNO_ENOMEM, ENOMEM)
+
+#define CUSTOM_ERRORS \
+        X(ERRNO_NONE, 0) \
+        X(ERRNO_SYNTAX, 1000) \
+        X(ERRNO_OVERFLOW, 1001) \
+
+#define X(name, val) name = val,
+enum Errors {
+        SYS_ERRORS
+        CUSTOM_ERRORS
+};
+#undef X
+
+int err_reportError(
+        ErrorFatality isErrFatal, int err,
+        const char* file, int line, const char* func);
 
 #endif
